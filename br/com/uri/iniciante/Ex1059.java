c2ef0440c681b146bfb0c5f12b0b0efdd174bbe2
vteodoro <viviteodoro19@gmail.com>
2018-05-14 23:52:22 -0300

    First commit

---
+++ br/com/uri/iniciante/Ex1059.java
@@ -0,0 +1,11 @@
+package br.com.uri.iniciante;
+
+public class Ex1059 {
+    public static void main (String[] args){
+        for (int i = 1; i <= 100 ; i++){
+            if(i %2 == 0){
+                System.out.println(i + "\n");
+            }
+        }
+    }
+}
