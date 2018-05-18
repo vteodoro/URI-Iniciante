60a0a12c9be2045d4e50be003d565d3eeae9ccb3
vteodoro <viviteodoro19@gmail.com>
2018-05-18 00:22:46 -0300

    edit ex1060

--- br/com/uri/iniciante/Ex1060.java
+++ br/com/uri/iniciante/Ex1060.java
@@ -1,5 +1,25 @@
 package br.com.uri.iniciante;
 
+import java.util.Scanner;
+
 public class Ex1060 {
+    
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int i = 0;
+        double[] j = new double[6];
+        int cont = 0;
+
+        while(i < 6){
+           j[i] = s.nextDouble();
+           if(j[i] > 0){
+                cont++;
+           }
+           i++;
+        }
+        
+        System.out.println(cont + " valores positivos");
+    
+    }
     
 }
