c979cb9bc2c443abc2fcdc48db77e688ac1bf7ee
vteodoro <viviteodoro19@gmail.com>
2018-05-21 22:32:56 -0300

    edit ex1065

--- br/com/uri/iniciante/Ex1065.java
+++ br/com/uri/iniciante/Ex1065.java
@@ -1,5 +1,21 @@
 package br.com.uri.iniciante;
 
+import java.util.Scanner;
+
 public class Ex1065 {
     
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int cont = 0;
+        for(int i = 0; i < 5; i++){
+            int[] x = new int[5];
+            x[i] = s.nextInt();
+            
+            if(x[i] %2 == 0){
+                cont ++;
+            }
+        }
+        
+        System.out.println(cont + " valores pares");
+    }
 }
