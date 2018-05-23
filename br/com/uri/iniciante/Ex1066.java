e8ae7c676de6f55380132b4653ed2c9549d0bde8
vteodoro <viviteodoro19@gmail.com>
2018-05-22 21:27:10 -0300

    add ex1066

---
+++ br/com/uri/iniciante/Ex1066.java
@@ -0,0 +1,9 @@
+package br.com.uri.iniciante;
+
+import java.util.Scanner;
+
+public class Ex1066 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+    }
+}
