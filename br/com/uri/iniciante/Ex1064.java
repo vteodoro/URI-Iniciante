c60e018f36b5ccd4abd50af2383a408516a37e9a
vteodoro <viviteodoro19@gmail.com>
2018-06-05 23:33:19 -0300

    edit ex1064

--- br/com/uri/iniciante/Ex1064.java
+++ br/com/uri/iniciante/Ex1064.java
@@ -1,5 +1,25 @@
 package br.com.uri.iniciante;
 
+import java.util.Scanner;
+
 public class Ex1064 {
+    public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    int cont = 0;
+    double media = 0;
+    for(int i = 0; i <  6; i++){
+        double[] x = new double[6];
+        x[i] = s.nextDouble();
+        if(x[i] >= 0){
+            cont ++;
+            media += x[i];
+        }    
+    }
     
+    media = media / cont;
+    
+    System.out.println(cont + " valores positivos");
+    System.out.println(media);
+    
+    }
 }
